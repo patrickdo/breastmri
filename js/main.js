c43e85971c3a379d7deba41aeaa78b9069c0026f
Patrick Do <patrickdo@gmail.com>
2015-01-01 08:35:38 -0800

    replicated bootstrap radio button functionality to avoid race condition. prelim report generation

--- js/main.js
+++ js/main.js
@@ -4,33 +4,73 @@
 "use strict";
 
 var b = {
-	lesions: ['focus', 'mass', 'nme']
-};
+		lesions: {
+			0: {
+				data: [],
+				text: ''
+			}
+		}
+	};
 
 $(document).ready(function() {
 
+b.update = function() {
+	b.getData();
+	$('#textareaReport').html(b.lesions[0].text);
+};
+
+b.getData = function() {
+	var lesionType = $('#lesion button.active').val();
+
+	if (lesionType === 'focus') {
+		//
+	} else if (lesionType === 'mass') {
+		b.lesions[0].text =
+			'There is ' + ($('#shape button.active').val() || '***') +
+			' mass with ' + ($('#margin button.active').val() || '***') +
+			' margins and ' + ($('#menht button.active').val() || '***') +
+			'.';
+	} else if (lesionType === 'nme') {
+		b.lesions[0].text =
+			'There is non-mass enhancement in ' + ($('#dist button.active').val() || '***') +
+			' that appears ' + ($('#t2 button.active').val() || '***') +
+			' on T2-weighted images, with ' + ($('#nenht button.active').val() || '***') +
+			' internal enhancement and exhibiting ' + ($('#initial button.active').val() || '***') +
+			' initial phase and ' + ($('#delayed button.active').val() || '***') +
+			' delayed phase kinetics.';
+	} else {
+		return '';
+	}
+};
 
 
 
 
-
-
-	$('#btnMass').click(function() {
-		$('#divFocus').hide('slow');
-		$('#divMass').show('slow');
-		$('#divNME').hide('slow');
-	});
-
-	$('#btnFocus').click(function() {
-		$('#divFocus').show('slow');
-		$('#divMass').hide('slow');
-		$('#divNME').hide('slow');
-	});
-
-	$('#btnNME').click(function() {
-		$('#divFocus').hide('slow');
-		$('#divMass').hide('slow');
-		$('#divNME').show('slow');
-	});
-
+$('#btnMass').click(function() {
+	$('#divFocus').hide('slow');
+	$('#divMass').show('slow');
+	$('#divNME').hide('slow');
 });
+
+$('#btnFocus').click(function() {
+	$('#divFocus').show('slow');
+	$('#divMass').hide('slow');
+	$('#divNME').hide('slow');
+});
+
+$('#btnNME').click(function() {
+	$('#divFocus').hide('slow');
+	$('#divMass').hide('slow');
+	$('#divNME').show('slow');
+});
+
+// manually replicating bootstrap functionality to avoid race condition
+$('button').click(function() {
+	$(this).addClass('active').siblings().removeClass('active');
+	$(this).blur();
+	b.update();
+});
+
+
+
+}); // END OF DOCUMENT.READY
