507685756e98a5b77beb81d9fa59255796644a69
Patrick Do <patrickdo@gmail.com>
2015-10-14 16:06:21 -0700

    add size/position. add lesion # labels.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
 var b = {
 		lesions: {
 			1: {
+				size: '',
+				loc: '',
 				type: '',
 				values: [],
 				text: ''
@@ -22,7 +24,7 @@ b.generateText = function() {
 	numLesions = Object.keys(b.lesions).length;
 	for (i = 0; i < numLesions; i++) {
 		//---MASS---
-		if (b.lesions[i+1].type === 'btnMass') {
+		if (b.lesions[i+1].type === 'Mass') {
 		// if (b.lesions[i+1].type === 'btnMass' && b.lesions[i+1].values.length > 0) {
 			var mT2='', mShape='', mMargin='', mIE='', mKI='', mKD='';
 
@@ -71,10 +73,10 @@ b.generateText = function() {
 			b.lesions[i+1].text =
 				'There is a T2 ' + (mT2 || '***') + ' ' + (mShape || '***') +
 				' mass with ' + (mMargin || '***') + ' margins and ' +
-				(mIE || '***') + ' . The mass exhibits ' + (mKI || '***') +
+				(mIE || '***') + '. The mass exhibits ' + (mKI || '***') +
 				' enhancement ' + (mKD || '***') + '.';
 		// ---NME---
-		} else if (b.lesions[i+1].type === 'btnNME') {
+		} else if (b.lesions[i+1].type === 'NME') {
 		// } else if (b.lesions[i+1].type === 'btnNME' && b.lesions[i+1].values.length > 0) {
 			var nT2='', nDist='', nIE='', nKI='', nKD='';
 
@@ -125,6 +127,17 @@ b.generateText = function() {
 		} else {
 			// return;
 		}
+
+		if (b.lesions[i+1].loc !== '') {
+			b.lesions[i+1].text = b.lesions[i+1].text
+				.replace(/(mass( enhancement)?)/, '$1 in the ' + b.lesions[i+1].loc);
+		}
+
+		if (b.lesions[i+1].size !== '') {
+			b.lesions[i+1].text = b.lesions[i+1].text
+				.replace(/(mass( enhancement)?)/, '$1 measuring ' + b.lesions[i+1].size);
+		}
+
 		report += (i+1) + ': ' + b.lesions[i+1].text + '<br>';
 	} // END CYCLE
 
@@ -137,14 +150,27 @@ b.generateText = function() {
 };
 
 b.clearButtons = function() {
-	$('.panel button').removeClass('active');
+
+	$('#lesionSection button, #lesionType button').removeClass('active');
+	$('#size-loc input').val('');
 };
 
 b.loadData = function() {
 	b.clearButtons();
 	var lesionNumber = $('#lesionList button.active').attr('id').substring(6);
 
-	$('#' + b.lesions[lesionNumber].type).addClass('active').siblings().removeClass('active');
+	// show/hide lesionType button and corresponding set of descriptors
+	if (b.lesions[lesionNumber].type !== '') {
+		$('#btn' + b.lesions[lesionNumber].type).addClass('active').siblings().removeClass('active');
+		$('#div' + b.lesions[lesionNumber].type).show().siblings().hide();
+		// put lesion number before Mass/NME
+		$('#lesionSection > div:visible legend span').text('Lesion ' + lesionNumber + ': ');
+	} else {	// if there is no b.lesion.type saved, unselect lesionType button and hide corresponding set of descriptors
+		$('#lesionType button').removeClass('active');
+		$('#lesionSection > div').hide('slow');
+	}
+
+	// cycle through each descriptor in the set and select the button if it matches the saved b.lesion.values
 	$('#lesionSection div:visible button').each(function(index) {
 		if (b.lesions[lesionNumber].values.indexOf(index) > -1) {
 			$(this).addClass('active').siblings().removeClass('active');
@@ -154,7 +180,13 @@ b.loadData = function() {
 
 b.saveData = function() {
 	var lesionNumber = $('#lesionList button.active').attr('id').substring(6);
-	b.lesions[lesionNumber].type = $('#lesionType button.active').attr('id');
+
+	// store lesion size/loc
+	b.lesions[lesionNumber].size = $('#inputSize').val();
+	b.lesions[lesionNumber].loc = $('#inputLocation').val();
+
+	// store lesion type
+	b.lesions[lesionNumber].type = $('#lesionType button.active').val();
 
 	// store descriptor buttons
 	b.lesions[lesionNumber].values = [];
@@ -163,13 +195,13 @@ b.saveData = function() {
 			b.lesions[lesionNumber].values.push(index);
 		}
 	});
-
 };
 
 // on clicking a lesion button. need to use .on() for dynamically added elements
 $('#lesionList').on('click', 'button', function() {
 	$(this).addClass('active')
 		.siblings().removeClass('active');
+	$(this).blur();
 	b.loadData();
 	b.generateText();
 });
@@ -181,11 +213,11 @@ $('.btn-group button').click(function() {
 		.siblings().removeClass('active');
 	$(this).blur();
 	if ((this.id === 'btnMass') || (this.id === 'btnNME')) {
-		$('#div'+this.id.substr(3,5)).show('slow')
-			.siblings().hide('slow');
+		$('#div'+this.id.substr(3,5)).show().siblings().hide();
 		$('#nt2 button,#dist button').removeClass('active');
 	}
 	b.saveData();
+	b.loadData();
 	b.generateText();
 });
 
@@ -198,11 +230,11 @@ $('#btnAddLesion').click(function() {
 	$('#lesionSection > div').hide('slow');
 
 	// add object data
-	b.lesions[lesionNumber] = {type: '', values: [], text: ''};
+	b.lesions[lesionNumber] = {size: '', loc: '', type: '', values: [], text: ''};
 
 	// add button
 	$('#lesionList').append(
-		'<button class="btn btn-primary lesions" type="radio" id="lesion' +
+		'<button class="btn btn-default lesions" type="radio" id="lesion' +
 		lesionNumber + '">#' + lesionNumber + '</button>'
 	);
 
@@ -290,6 +322,13 @@ $(".hover").mousemove(function (e) {
 		.css("top", (e.pageY - 15) + "px")
 		.css("left", (e.pageX - 20) + "px");
 });
+
+// auto-update size/loc
+$('body').on('keyup', 'input', function() {
+	b.saveData();
+	b.generateText();
+});
+
 
 b.generateText();
 
